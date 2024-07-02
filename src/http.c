852891b0cf0c4bb9d2a8aec5f97dc1e60a7a6a27
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-02 14:46:04 +0700

    init sock and cleanup func

---
+++ src/http.c
@@ -0,0 +1,44 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "http.h"
+
+static void init_sock_fd(struct http_stack *http_stack) 
+{
+    int fd;
+
+    fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    if (fd < 0) {
+        perror("socket()");
+        _exit(-1);
+    }
+
+    http_stack->sockfd = fd;
+}
+
+static void do_work(struct http_stack *http_stack) {
+    while (1) {
+        
+    }
+}
+
+
+static void cleanup(struct http_stack *http_stack) {
+    close(http_stack->sockfd);
+}
+
+int main() 
+{
+    struct http_stack http_stack;
+    
+    init_sock_fd(&http_stack);
+
+    // eventloop
+    do_work(&http_stack);
+    cleanup(&http_stack);
+    return 0;
+}
---
+++ src/http.h
@@ -0,0 +1,8 @@
+#ifndef HTTP_H
+#define HTTP_H
+
+struct http_stack {
+    int sockfd;
+};
+
+#endif
